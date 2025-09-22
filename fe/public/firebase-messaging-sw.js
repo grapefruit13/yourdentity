fcfa621eadb9a79f9e425a46cbc73c3777babb5e
grapefruit <liam.heo11@gmail.com>
2025-09-22 16:39:42 +0900

    fix: firbase messaging sw-config값 객체에 하드코딩(보안위험X)

--- fe/public/firebase-messaging-sw.js
+++ fe/public/firebase-messaging-sw.js
@@ -5,13 +5,13 @@ importScripts(
 
 // Replace these with your own Firebase config keys...
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDrUoph1tb6UeIPiEcUjyaolThcxWKbHy0",
+  authDomain: "youthvoice-2025.firebaseapp.com",
+  projectId: "youthvoice-2025",
+  storageBucket: "youthvoice-2025.firebasestorage.app",
+  messagingSenderId: "700631058497",
+  appId: "1:700631058497:web:2160361e1c25bec8bf1ec0",
+  measurementId: "G-HXQ41FTC7V",
 };
 
 firebase.initializeApp(firebaseConfig);
