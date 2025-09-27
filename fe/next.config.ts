55be3323773c9308707145555c55476c5586021e
grapefruit <liam.heo11@gmail.com>
2025-09-27 13:17:23 +0900

    ddfdd:fdfd

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -3,9 +3,10 @@ import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   output: "export",
-
+  trailingSlash: true,
   images: {
     formats: ["image/webp", "image/avif"],
+    unoptimized: true, // 정적 export에서 필요
   },
 };
 
