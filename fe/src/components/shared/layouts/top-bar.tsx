9fb9959997efdec800ecb9cbe7bef5995f13179d
grapefruit <liam.heo11@gmail.com>
2025-10-01 13:29:42 +0900

    feat: topbar - 뒤로가기 버튼 handler 연결

--- fe/src/components/shared/layouts/top-bar.tsx
+++ fe/src/components/shared/layouts/top-bar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { IMAGE_URL } from "@/constants/shared/_image-url";
 
 const TITLE_MAP: Array<{ prefix: string; label: string }> = [
+  { prefix: "/mission", label: "미션" },
   { prefix: "/community", label: "커뮤니티" },
   { prefix: "/my-page", label: "마이" },
 ];
@@ -14,17 +15,27 @@ const TITLE_MAP: Array<{ prefix: string; label: string }> = [
  */
 const TopBar = () => {
   const pathname = usePathname();
+  const router = useRouter();
   const currentTitle =
     TITLE_MAP.find((item) => pathname?.startsWith(item.prefix))?.label ?? "";
+
+  const handleClick = () => {
+    router.back();
+  };
+
   return (
     <div className="sticky top-0 left-0 flex h-12 w-full items-center justify-center bg-slate-100">
-      <Image
-        src={IMAGE_URL.ICON.chevron.left.url}
-        alt={IMAGE_URL.ICON.chevron.left.alt}
-        width={20}
-        height={20}
-        className="absolute left-4"
-      />
+      <button
+        onClick={handleClick}
+        className="absolute left-4 hover:cursor-pointer"
+      >
+        <Image
+          src={IMAGE_URL.ICON.chevron.left.url}
+          alt={IMAGE_URL.ICON.chevron.left.alt}
+          width={20}
+          height={20}
+        />
+      </button>
       <span>{currentTitle}</span>
     </div>
   );
