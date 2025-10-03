a1aea7f91a7e9ba3e85e1864093d3245442f1e51
grapefruit <liam.heo11@gmail.com>
2025-10-03 20:19:20 +0900

    feat: 이메일 로그인 기능 구현

--- fe/src/app/(auth)/email-login/page.tsx
+++ fe/src/app/(auth)/email-login/page.tsx
@@ -1,38 +1,59 @@
 "use client";
 
+import { FormEvent, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import ButtonBase from "@/components/shared/base/button-base";
-import InputBase from "@/components/shared/base/input-base";
+import Input from "@/components/shared/input";
 import { NBody1B, NBody2M, NLabel1M } from "@/components/shared/typography";
 import { IMAGE_URL } from "@/constants/shared/_image-url";
 import { LINK_URL } from "@/constants/shared/_link-url";
 import useToggle from "@/hooks/shared/useToggle";
+import { cn } from "@/utils/shared/cn";
 
 /**
  * @description 이메일 로그인 페이지
  */
 const EmailLoginPage = () => {
   const { isOpen, toggle } = useToggle();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const isLoginValidate = email && password;
+
+  /**
+   * @description 로그인 제출
+   * @param e - 폼 이벤트
+   */
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert("로그인 제출");
+    // TODO: 메인 홈으로 이동. 현재는 미정.
+    router.push(LINK_URL.MISSION);
+  };
 
   return (
-    <div className="flex h-full flex-col bg-white px-5 py-6">
+    <form
+      className="flex h-full flex-col bg-white px-5 py-6"
+      onSubmit={handleSubmit}
+    >
       <div className="flex h-fit flex-col gap-6">
         <div className="flex flex-col gap-3">
           <NBody2M>아이디 (이메일)</NBody2M>
-          <InputBase
+          <Input
             type="email"
             placeholder="이메일을 입력하세요"
-            className="font-noto rounded-md border border-gray-200 px-3 py-2 text-base leading-1.5 font-normal shadow-xs"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="flex flex-col gap-3">
           <NBody2M>비밀번호</NBody2M>
           <div className="relative">
-            <InputBase
+            <Input
               type={isOpen ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="비밀번호를 입력하세요"
-              className="font-noto w-full rounded-md border border-gray-200 px-3 py-2 pr-10 text-base leading-1.5 font-normal shadow-xs"
             />
             <ButtonBase type={"button"} onClick={toggle}>
               <Image
@@ -62,10 +83,17 @@ const EmailLoginPage = () => {
           <NLabel1M className="text-gray-400">이메일 회원가입</NLabel1M>
         </Link>
       </div>
-      <ButtonBase className="bg-primary-600 mt-auto w-full rounded-lg py-2 opacity-70">
+      <ButtonBase
+        type="submit"
+        disabled={!isLoginValidate}
+        className={cn(
+          "bg-primary-600 mt-auto w-full rounded-lg py-2",
+          !isLoginValidate && "opacity-50"
+        )}
+      >
         <NBody1B className="text-white">로그인</NBody1B>
       </ButtonBase>
-    </div>
+    </form>
   );
 };
 
