65d3f33b4e0e0e60b10c2740e9f894d42e5912df
hyerin315 <lhl9603@gmail.com>
2025-10-03 22:52:52 +0900

    chore: 공백 추가

--- fe/src/app/(auth)/forget-password/page.tsx
+++ fe/src/app/(auth)/forget-password/page.tsx
@@ -2,7 +2,7 @@
  * @description 비밀번호 재설정 페이지
  */
 const ForgetPasswordPage = () => {
-    return <h1>ForgetPasswordPage</h1>;
-  };
-  
-  export default ForgetPasswordPage;
+  return <h1>ForgetPasswordPage</h1>;
+};
+
+export default ForgetPasswordPage;
