36b059fbf4f763fc8d5fb7794ac4b088450dee14
grapefruit <liam.heo11@gmail.com>
2025-10-03 20:18:29 +0900

    feat: 비밀번호 재설정, 카카오 로그인, 약관 페이지 추가

---
+++ fe/src/app/(auth)/kakao-login/page.tsx
@@ -0,0 +1,8 @@
+/**
+ * @description 카카오 로그인 페이지
+ */
+const KakaoLoginPagepage = () => {
+  return <div>KakaoLoginPagepage</div>;
+};
+
+export default KakaoLoginPagepage;
---
+++ fe/src/app/(auth)/terms-of-service/page.tsx
@@ -0,0 +1,8 @@
+/**
+ * @description 이용약관 페이지
+ */
+const TermsOfService = () => {
+  return <div>TermsOfService</div>;
+};
+
+export default TermsOfService;
