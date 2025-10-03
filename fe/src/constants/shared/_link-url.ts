ebe6ed10bc1c17fc5b9bc90b11521df4326ff39c
grapefruit <liam.heo11@gmail.com>
2025-10-03 19:05:57 +0900

    feat: link url constant 정의

---
+++ fe/src/constants/shared/_link-url.ts
@@ -0,0 +1,8 @@
+export const LINK_URL = {
+  KAKAO_LOGIN: "/login/kakao",
+  EMAIL_LOGIN: "/email-login",
+  TERMS_OF_SERVICE: "/login/terms-of-service",
+  PRIVACY_POLICY: "/login/privacy-policy",
+  FIND_PASSWORD: "/login/find-password",
+  EMAIL_SIGNUP: "/signup/email",
+};
