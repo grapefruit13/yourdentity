90e75b3d44b4e5f06bc64d04dfb51a96da93283a
grapefruit <liam.heo11@gmail.com>
2025-10-09 23:13:40 +0900

    fix: main layout-auth guard 해제

--- fe/src/app/(main)/layout.tsx
+++ fe/src/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import BottomNavigation from "@/components/shared/layouts/bottom-navigation";
 import TopBar from "@/components/shared/layouts/top-bar";
-import { AuthGuard } from "@/contexts/shared/guard";
 /**
  * @description 하단 네브바 포함 레이아웃
  */
@@ -10,12 +9,10 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AuthGuard>
-      <div className="flex min-h-[100dvh] w-full flex-col">
-        <TopBar />
-        <main className="flex w-full flex-1 flex-col">{children}</main>
-        <BottomNavigation />
-      </div>
-    </AuthGuard>
+    <div className="flex min-h-[100dvh] w-full flex-col">
+      <TopBar />
+      <main className="flex w-full flex-1 flex-col">{children}</main>
+      <BottomNavigation />
+    </div>
   );
 }
