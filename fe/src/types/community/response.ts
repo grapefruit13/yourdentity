03ab1052674fc3a1d3200cded99b540c7c219252
grapefruit <liam.heo11@gmail.com>
2025-10-16 13:39:32 +0900

    feat: community 목록조회, 포스트조회 타입 정의 및 함수 구현

---
+++ fe/src/types/community/response.ts
@@ -0,0 +1,18 @@
+// TODO: be response 수정 예정
+export interface GETCommunityListRes {
+  pagination: {
+    pageNumber: number;
+    pageSize: number;
+    totalElements: number;
+    totalPages: number;
+    hasNext: boolean;
+    hasPrevious: boolean;
+    isFirst: boolean;
+    isLast: boolean;
+  };
+}
+
+// TODO: be response 수정 예정
+export interface GETCommunityPostListRes {
+  pagination: "";
+}
