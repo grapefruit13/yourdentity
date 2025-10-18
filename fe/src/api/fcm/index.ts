fa9afbab5948d57aacc40fa7574782d3144e96cc
Gijin Park <dugudada01@naver.com>
2025-10-18 16:17:55 +0900

    refactor: FCM API 구조 표준화 및 린트 에러 수정

--- fe/src/api/fcm/index.ts
+++ fe/src/api/fcm/index.ts
@@ -1,6 +1,7 @@
 /**
  * @description FCM 토큰 관련 API 함수
  */
+import { API_PATH } from "@/constants/shared/_api-path";
 import { post } from "@/lib/axios";
 import { FCMTokenRequest, FCMTokenResponse } from "@/types/shared/fcm";
 import { Result } from "@/types/shared/response";
@@ -8,13 +9,16 @@ import { Result } from "@/types/shared/response";
 /**
  * @description FCM 토큰 저장/업데이트
  */
-export const saveFCMToken = async (
+const saveToken = async (
   data: FCMTokenRequest
 ): Promise<Result<FCMTokenResponse>> => {
-  try {
-    const response = await post<Result<FCMTokenResponse>>("/fcm/token", data);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await post<Result<FCMTokenResponse>>(
+    API_PATH.FCM.TOKEN,
+    data
+  );
+  return response.data;
+};
+
+export const fcmApi = {
+  saveToken,
 };
