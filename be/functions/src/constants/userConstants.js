ed17befeccdb5cc061fca873e0769da739e4d4cb
hyerin315 <lhl9603@gmail.com>
2025-10-31 02:06:30 +0900

    feat: Users 컬렉션 상수 추가

--- be/functions/src/constants/userConstants.js
+++ be/functions/src/constants/userConstants.js
@@ -9,7 +9,26 @@ const SNS_PROVIDERS = {
   KAKAO: "kakao",
 };
 
+// 사용자 상태
+const USER_STATUS = {
+  ACTIVE: "active",
+  INACTIVE: "inactive",
+};
+
+// 자격 정지 상태
+const SUSPENSION_TYPES = {
+  TEMPORARY: "temporary",
+  PERMANENT: "permanent",
+  ACTIVE: "active", // 제재 미적용 상태 의미
+};
+
+// 기본 업로드 쿼터 (1GB)
+const DEFAULT_UPLOAD_QUOTA_BYTES = 1073741824;
+
 module.exports = {
   AUTH_TYPES,
   SNS_PROVIDERS,
+  USER_STATUS,
+  SUSPENSION_TYPES,
+  DEFAULT_UPLOAD_QUOTA_BYTES,
 };
