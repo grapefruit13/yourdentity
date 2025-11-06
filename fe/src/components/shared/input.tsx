4a8a5d28369c2f3adba43de27dccfd655e47e4e5
Seongjun Kim <lewisel@naver.com>
2025-11-06 09:19:54 +0900

    fix(Input): readOnly일 때 포커스 잡히지 않도록 수정

--- fe/src/components/shared/input.tsx
+++ fe/src/components/shared/input.tsx
@@ -4,13 +4,23 @@ import { cn } from "@/utils/shared/cn";
 /**
  * @description 디자인 시스템 input
  */
-const Input = ({ className, ...props }: ComponentProps<"input">) => {
+const Input = ({ className, readOnly, ...props }: ComponentProps<"input">) => {
   return (
     <input
       className={cn(
         "font-noto focus:ring-primary-400 focus:outline-primary-400 focus:border-primary-600 w-full rounded-md border border-gray-200 px-3 py-2 pr-10 text-base leading-1.5 font-normal shadow-xs focus:outline-3",
         className
       )}
+      readOnly={readOnly}
+      tabIndex={readOnly ? -1 : undefined}
+      onMouseDown={
+        readOnly
+          ? (e) => {
+              // readOnly일 경우 마우스 클릭으로 포커스가 잡히지 않도록 방지
+              e.preventDefault();
+            }
+          : undefined
+      }
       {...props}
     />
   );
