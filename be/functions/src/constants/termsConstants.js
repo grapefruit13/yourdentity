376077202fc6ed5d32de21ca1a522ddaec0d30ed
hyerin315 <lhl9603@gmail.com>
2025-11-09 16:28:05 +0900

    fix: 상수 수정

--- be/functions/src/constants/termsConstants.js
+++ be/functions/src/constants/termsConstants.js
@@ -8,10 +8,10 @@ const TERMS_VERSIONS = {
 
 // 카카오 간편가입 약관 태그 (카카오 콘솔 설정과 일치해야 함)
 const TERMS_TAGS = {
-  SERVICE: "terms_service_v1",
-  PRIVACY: "terms_privacy_v1",
-  AGE14: "user_age_check", // 만 14세 이상 고정 태그
-  PUSH: "terms_push_v1",
+  SERVICE: "terms_service",     // 카카오 API: "terms_service"
+  PRIVACY: "terms_privacy",     // 카카오 API: "terms_privacy"
+  AGE14: "user_age_check",      // 카카오 API: "user_age_check" (고정)
+  PUSH: "terms_push",           // 카카오 API: "terms_push"
 };
 
 module.exports = {
