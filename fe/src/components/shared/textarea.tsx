5e1c26996c49da18ad441da169cac975caff6a22
grapefruit <liam.heo11@gmail.com>
2025-11-12 21:37:02 +0900

    feat: textarea 공통 컴포넌트 구현

---
+++ fe/src/components/shared/textarea.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps } from "react";
+import { cn } from "@/utils/shared/cn";
+
+/**
+ * @description 디자인 시스템 textarea
+ */
+const Textarea = ({
+  className,
+  readOnly,
+  ...props
+}: ComponentProps<"textarea">) => {
+  return (
+    <textarea
+      className={cn(
+        "font-noto focus:ring-main-400 focus:outline-main-400 focus:border-main-600 h-21 w-full resize-none rounded-md border border-gray-200 bg-white px-3 py-4 text-base leading-1.5 font-normal text-gray-900 shadow-xs focus:outline-3",
+        className
+      )}
+      readOnly={readOnly}
+      tabIndex={readOnly ? -1 : undefined}
+      onMouseDown={
+        readOnly
+          ? (e) => {
+              // readOnly일 경우 마우스 클릭으로 포커스가 잡히지 않도록 방지
+              e.preventDefault();
+            }
+          : undefined
+      }
+      {...props}
+    />
+  );
+};
+
+export default Textarea;
