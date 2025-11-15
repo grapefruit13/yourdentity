7c0e4ea4470997faedb76d3a841ecc80b1bd57f0
grapefruit <liam.heo11@gmail.com>
2025-11-16 00:26:58 +0900

    feat: 미션 홈페이지 ui구현

--- fe/src/app/(main)/mission/page.tsx
+++ fe/src/app/(main)/mission/page.tsx
@@ -1,51 +1,82 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { ChevronRight, Info, Loader, Target } from "lucide-react";
+import { ChevronRight, Target } from "lucide-react";
+import { ActiveMissionCard } from "@/components/mission/active-mission-card";
 import { MissionCertificationCard } from "@/components/mission/mission-certification-card";
-import ButtonBase from "@/components/shared/base/button-base";
+import { MissionRecommendationCard } from "@/components/mission/mission-recommendation-card";
+import { RecommendedMissionCard } from "@/components/mission/recommended-mission-card";
 import { Typography } from "@/components/shared/typography";
 import { Button } from "@/components/shared/ui/button";
+import { InfoIconWithTooltip } from "@/components/shared/ui/info-icon-with-tooltip";
+import { MoreButton } from "@/components/shared/ui/more-button";
 import { ProgressGauge } from "@/components/shared/ui/progress-gauge";
 import { LINK_URL } from "@/constants/shared/_link-url";
+import { getFutureDate } from "@/utils/shared/date";
 
 /**
  * @description 미션 페이지
  */
-// TODO: 진행하고 있는 미션 유무에 따라 문구랑 카드 내용 바뀜
 const Page = () => {
   const router = useRouter();
+
+  // TODO: 실 api response로 교체
+  const isOnMission = true;
+  const remainingMission = 3;
 
   return (
     <div className="h-full min-h-screen bg-gray-200">
       <div className="p-5">
         <Typography
+          as="span"
           font="noto"
           variant="heading2B"
           className="pb-4 text-gray-950"
         >
-          오늘의 미션을 시작해 볼까요?
+          {isOnMission
+            ? `오늘의 미션이 ${remainingMission}개 남았어요!`
+            : `오늘의 미션을 시작해 볼까요?`}
         </Typography>
-        <div className="mt-4 mb-1 flex w-full flex-col items-center justify-center rounded-lg bg-white py-6">
-          <Loader className="mb-2 h-6 w-6 text-gray-400" />
-          <Typography
-            font="noto"
-            variant="body1B"
-            className="text-center text-gray-950"
-          >
-            진행 중인 미션이 없어요. <br />
-            미션을 시작해 보세요!
-          </Typography>
-        </div>
+        {/* 흰카드 */}
+        {isOnMission ? (
+          <div className="scrollbar-hide mt-4 flex w-[calc(100%+1.25rem)] gap-3 overflow-x-auto overflow-y-hidden pr-5">
+            <ActiveMissionCard
+              title="친구와 함께 요리하기"
+              tags={["건강한 아침 러닝 30분 하기", "요리", "친구와 함께"]}
+              endTime={getFutureDate(7, 32, 12)}
+              onDelete={() => {
+                // TODO: 미션 삭제 로직
+              }}
+            />
+            <ActiveMissionCard
+              title="친구와 함께 요리하기"
+              tags={["건강한 아침 러닝 30분 하기", "요리", "친구와 함께"]}
+              endTime={getFutureDate(7, 32, 12)}
+              onDelete={() => {
+                // TODO: 미션 삭제 로직
+              }}
+            />
+            <ActiveMissionCard
+              title="친구와 함께 요리하기"
+              tags={["건강한 아침 러닝 30분 하기", "요리", "친구와 함께"]}
+              endTime={getFutureDate(7, 32, 12)}
+              onDelete={() => {
+                // TODO: 미션 삭제 로직
+              }}
+            />
+          </div>
+        ) : (
+          <MissionRecommendationCard message="진행 중인 미션이 없어요. 미션을 시작해 보세요!" />
+        )}
 
         <Button
           variant="default"
           size="default"
-          className="w-full rounded-lg"
+          className="mt-1 w-full rounded-lg"
           onClick={() => router.push(LINK_URL.MISSION_LIST)}
         >
           <Typography font="noto" variant="body3B" className="text-white">
-            미션 보러가기
+            {isOnMission ? "미션 인증하기" : "미션 보러가기"}
           </Typography>
           <ChevronRight className="h-4 w-4 text-white" />
         </Button>
@@ -81,7 +112,7 @@ const Page = () => {
               >
                 연속 미션일
               </Typography>
-              <Info className="size-3 text-gray-400" />
+              <InfoIconWithTooltip message="하루도 빠짐없이 매일 미션을 인증한 연속 일수입니다." />
             </div>
             <div className="flex items-center gap-1">
               <Typography
@@ -112,7 +143,7 @@ const Page = () => {
               >
                 진행 미션 수
               </Typography>
-              <Info className="size-3 text-gray-400" />
+              <InfoIconWithTooltip message="현재 진행 중인 미션의 개수입니다." />
             </div>
             <div className="flex items-center gap-1">
               <Typography
@@ -132,39 +163,91 @@ const Page = () => {
             </div>
           </div>
         </div>
-        {/* 친구들이 인증한 미션이에요 */}
-        <div className="mt-9 mb-5 flex w-full items-center justify-between">
-          <Typography font="noto" variant="heading3B" className="text-gray-950">
-            친구들이 인증한 미션이에요
-          </Typography>
-          <ButtonBase
-            className="flex items-center justify-center gap-1"
-            onClick={() => router.push(LINK_URL.COMMUNITY)}
-          >
-            <Typography font="noto" variant="body3R" className="text-gray-400">
-              더 보기
-            </Typography>
-            <ChevronRight className="size-3 text-gray-400" />
-          </ButtonBase>
-        </div>
-        {/* 후기 슬라이딩 */}
-        <div className="scrollbar-hide -mx-5 flex gap-3 overflow-x-auto overflow-y-hidden px-5">
-          {/* TODO: 카드 클릭 시 미션 인증이 올라간 피드 상세로 이동 */}
-          <MissionCertificationCard
-            title="오늘 하늘이 이뻤어요!"
-            thumbnailText="두줄까지 미리보기로 보이게!! 구름이 뭉게뭉게 있어서 하늘이 이뻐요! 두줄까지 미리보기로 보이게!! 구름이 뭉게뭉게 있어서 하늘이 이뻐요!"
-            thumbnailImageUrl="/imgs/mockup3.jpg"
-            tagName="건강한 아침 러닝 30분 하기"
-            postId="1"
-          />
-          <MissionCertificationCard
-            title="오늘 하늘이 이뻤어요!"
-            thumbnailText="두줄까지 미리보기로 보이게!! 구름이 뭉게뭉게 있어서 하늘이 이뻐요! 두줄까지 미리보기로 보이게!! 구름이 뭉게뭉게 있어서 하늘이 이뻐요!"
-            thumbnailImageUrl="/imgs/mockup3.jpg"
-            tagName="건강한 아침 러닝 30분 하기"
-            postId="1"
-          />
-        </div>
+        {/* 미션 진행중인거 없을때 */}
+        {!isOnMission && (
+          <>
+            {/* 친구들이 인증한 미션이에요 */}
+            <div className="mt-9 mb-5 flex w-full items-center justify-between">
+              <Typography
+                font="noto"
+                variant="heading3B"
+                className="text-gray-950"
+              >
+                친구들이 인증한 미션이에요
+              </Typography>
+              <MoreButton onClick={() => router.push(LINK_URL.COMMUNITY)} />
+            </div>
+            {/* 후기 슬라이딩 */}
+            <div className="scrollbar-hide -mx-5 flex gap-3 overflow-x-auto overflow-y-hidden px-5">
+              {/* TODO: 카드 클릭 시 미션 인증이 올라간 피드 상세로 이동 */}
+              <MissionCertificationCard
+                title="오늘 하늘이 이뻤어요!"
+                thumbnailText="두줄까지 미리보기로 보이게!! 구름이 뭉게뭉게 있어서 하늘이 이뻐요! 두줄까지 미리보기로 보이게!! 구름이 뭉게뭉게 있어서 하늘이 이뻐요!"
+                thumbnailImageUrl="/imgs/mockup3.jpg"
+                tagName="건강한 아침 러닝 30분 하기"
+                postId="1"
+              />
+              <MissionCertificationCard
+                title="오늘 하늘이 이뻤어요!"
+                thumbnailText="두줄까지 미리보기로 보이게!! 구름이 뭉게뭉게 있어서 하늘이 이뻐요! 두줄까지 미리보기로 보이게!! 구름이 뭉게뭉게 있어서 하늘이 이뻐요!"
+                thumbnailImageUrl="/imgs/mockup3.jpg"
+                tagName="건강한 아침 러닝 30분 하기"
+                postId="1"
+              />
+            </div>
+          </>
+        )}
+        {/* 미션 진행 중인거 있을때  */}
+        {isOnMission && (
+          <>
+            <div className="mt-9 mb-5 flex w-full items-center justify-between">
+              <Typography
+                font="noto"
+                variant="heading3B"
+                className="text-gray-950"
+              >
+                다음 미션으로 이건 어때요?
+              </Typography>
+              <MoreButton onClick={() => router.push(LINK_URL.MISSION_LIST)} />
+            </div>
+            {/* 후기 슬라이딩 */}
+            <div className="scrollbar-hide -mx-5 flex gap-3 overflow-x-auto overflow-y-hidden px-5">
+              {/* TODO: 카드 클릭 시 미션 상세로 이동 */}
+              <RecommendedMissionCard
+                title="주말 자전거 타기 1시간 하기"
+                tagName="자전거"
+                likeCount={15}
+                onClick={() => {
+                  // TODO: 미션 상세로 이동
+                }}
+              />
+              <RecommendedMissionCard
+                title="주말 자전거 타기 1시간 하기"
+                tagName="자전거"
+                likeCount={15}
+                onClick={() => {
+                  // TODO: 미션 상세로 이동
+                }}
+              />
+              <RecommendedMissionCard
+                title="주말 자전거 타기 1시간 하기"
+                tagName="자전거"
+                likeCount={15}
+                onClick={() => {
+                  // TODO: 미션 상세로 이동
+                }}
+              />
+              <RecommendedMissionCard
+                title="주말 자전거 타기 1시간 하기"
+                tagName="자전거"
+                likeCount={15}
+                onClick={() => {
+                  // TODO: 미션 상세로 이동
+                }}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
