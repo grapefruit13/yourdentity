32c548bdafb82a6f4e41c849ce4614e6994b90df
seongjun-kim <lewisel@naver.com>
2025-11-25 21:00:48 +0900

    fix: cacheStorage 활용 방안 적용
    ref#1: https://stackoverflow.com/questions/62669966/how-to-maintain-login-status-in-a-pwa-initially-loaded-via-safari-14-ios-14
    
    ref#2: https://www.google.com/search?q=pinterest+pwa+login+redirect&rlz=1CDGOYI_enKR866KR869&hl=ko&sourceid=chrome-mobile&ie=UTF-8

--- fe/src/app/manifest.ts
+++ fe/src/app/manifest.ts
@@ -1,10 +1,22 @@
 import type { MetadataRoute } from "next";
 
+/**
+ * @description iOS PWA 매니페스트 설정
+ *
+ * iOS PWA에서 로그인 상태 유지 방법:
+ * - iOS 14+에서는 Safari와 PWA가 cacheStorage를 공유
+ * - cacheStorage를 활용하여 인증 정보를 저장하고 복원
+ * - start_url을 "/"로 설정하여 기본 경로에서 시작
+ *
+ * 참고:
+ * - https://stackoverflow.com/questions/62669966/how-to-maintain-login-status-in-a-pwa-initially-loaded-via-safari-14-ios-14
+ */
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "유스잇",
     short_name: "유스잇",
     description: "유스잇",
+    // PWA 시작 URL - 홈 화면에서 앱을 열 때 로드되는 기본 경로
     start_url: "/",
     display: "standalone",
     orientation: "portrait",
